97a51936656ca3df5564ce22b04c95b53d88999b
YoungAndDo <yd199323@gmail.com>
2018-12-01 18:06:14 +0900

    컴포넌트 생성, ngFor

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes'; // named import 방법
+
+@Component({ // 여기가 METADATA 부분, 데코레이트 패턴
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'WinStorm'
+  // }
+  hero: Hero;
+  isSpecial = true;
+  heroes = HEROES;
+
+  constructor() {
+    this.hero = new Hero();
+    this.hero.id = 1;
+    this.hero.name = 'WinStorm';
+  }
+
+  ngOnInit() {
+  }
+
+  onSave(e) {
+    console.log(e);
+    alert('he');
+  }
+}
